e35bd63e0bcef1a785e1ff080a7cdb5c31fb87c7
kalebbecannon <kalebbecannon@gmail.com>
2023-03-03 12:43:20 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class NumericTypes
+{
+    public static void Main()
+    {
+        int L=10;
+        int W=9;
+        int a=L*W;
+
+        Console.WriteLine("The area of your rectangle is : "+a);
+
+    }
+}
